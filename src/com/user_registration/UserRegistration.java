ae5d289baa4565dab614c56033ced29f90b74ecc
Your Name <you@example.com>
2020-10-22 17:47:06 +0530

    Added: Email validation

--- src/com/user_registration/UserRegistration.java
+++ src/com/user_registration/UserRegistration.java
@@ -27,9 +27,21 @@ public class UserRegistration {
 			System.out.println("Invalid");
 	}
 
+	public static void email() {
+		System.out.println("Email : ");
+		String email = SC.next();
+		boolean mail = Pattern.matches("^[A-Za-z0-9]+([._%+-][0-9a-zA-Z]+)*@[A-Za-z0-9]+.[A-Za-z]{2,4}([.][a-zA-Z]{1,3})*$", email);
+		if(mail == true)
+			System.out.println("Valid");
+		else
+			System.out.println("Invalid");
+	}
+
+
 	public static void main(String args[]) {
 		System.out.println("*****WELCOME TO USER REGISTRATION SYSTEM*****");
 		firstName();
 		lastName();
+		email();
 	}
 }
