4de95e902f6071c764fa3407b4cb6b632a8a00a1
Your Name <you@example.com>
2020-10-22 17:27:24 +0530

    Added: First Name Validation

--- src/com/user_registration/UserRegistration.java
+++ src/com/user_registration/UserRegistration.java
@@ -1,7 +1,24 @@
 package com.user_registration;
 
+import java.util.Scanner;
+import java.util.regex.*;
+
+
 public class UserRegistration {
+	private static final Scanner SC = new Scanner(System.in);
+
+	public static void firstName() {
+		System.out.println("First Name : ");
+		String firstName = SC.next();
+		boolean fname = Pattern.matches("^[A-Z]([a-z]{2,})$", firstName);
+		if(fname == true)
+			System.out.println("Valid");
+		else 
+			System.out.println("Invalid");
+	}
+
 	public static void main(String args[]) {
 		System.out.println("*****WELCOME TO USER REGISTRATION SYSTEM*****");
+		firstName();
 	}
 }
