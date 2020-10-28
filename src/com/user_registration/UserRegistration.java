46bd0085a55c20481b5064518acb8039e76cefbd
Your Name <you@example.com>
2020-10-28 17:10:54 +0530

    Refactored: All methods using Lambda Expression

--- src/com/user_registration/UserRegistration.java
+++ src/com/user_registration/UserRegistration.java
@@ -3,84 +3,80 @@ package com.user_registration;
 import java.util.Scanner;
 import java.util.regex.*;
 
+@FunctionalInterface
+interface IUserRegistration {
+	String validate();
+}
 
 public class UserRegistration {
+
 	private static final Scanner SC = new Scanner(System.in);
 
-	public static void firstName() {
-		System.out.println("First Name : ");
-		String firstName = SC.next();
-		boolean fname = Pattern.matches("^[A-Z]([a-z]{2,})$", firstName);
-		if(fname == true)
-			System.out.println("Valid");
-		else
-			System.out.println("Invalid");
-	}
-
-	public static void lastName() {
-		System.out.println("Last Name : ");
-		String lastName = SC.next();
-		boolean lname = Pattern.matches("^[A-Z]([a-z]{2,})$", lastName);
-		if(lname == true)
-			System.out.println("Valid");
-		else
-			System.out.println("Invalid");
-	}
-
-	public static void email() {
-		System.out.println("Email : ");
-		String email = SC.next();
-		boolean mail = Pattern.matches("^[A-Za-z0-9]+([._%+-][0-9a-zA-Z]+)*@[A-Za-z0-9]+([.]([a-zA-Z]{2,3})*)+$", email);
-		if(mail == true)
-			System.out.println("Valid");
-		else
-			System.out.println("Invalid");
-	}
-
-	public static void mobileNum() {
-		System.out.println("Mobile Number : ");
-		String mobile = SC.next();
-		boolean mobileNo = Pattern.matches("^[+]*([0-9]{1,3})+([[:space:]])+[0-9]{10}$", mobile);
-		if(mobileNo == true)
-			System.out.println("Valid");
-		else
-			System.out.println("Invalid");
-	}
-
-	public static void password() {
-		System.out.println("Password : ");
-		String pwd = SC.next();
-		boolean pwd1 = Pattern.matches("^([0-9a-zA-Z@#!]){8,}$", pwd);
-		boolean pwd2 = Pattern.matches("^([a-z0-9@#!]*)[A-Z]+([a-z0-9@#!]*)$", pwd);
-		boolean pwd3 = Pattern.matches("^([a-zA-Z@#!]*)[0-9]+([a-zA-Z@#!]*)$", pwd);
-		boolean pwd4 = Pattern.matches("^([a-zA-Z0-9]*)[@#!]{1}([a-zA-Z0-9]*)$", pwd);
-		if(pwd1 == true) {
-			if(pwd2 == true) {  
-				if(pwd3 == true) {
-					if(pwd4 == true) {
-						System.out.println("Rule 1,2,3 & 4 Matched");
-					}else {
-						System.out.println("Rule 4 Not Matched");
-					}}else {
-					System.out.println("Rule 3 Not Matched");
-				}} else { 
-				System.out.println("Rule 2 Not Matched");
-			}} else {
-			System.out.println("Rule 1 Not Matched");
-		}
-	}
-	
 	public static void main(String args[]) {
 		System.out.println("*****WELCOME TO USER REGISTRATION SYSTEM*****");
-		firstName();
-		lastName();
-		email();
-		mobileNum();
-		System.out.println("Rules to be maintained for attaining a valid password");
+
+		IUserRegistration firstName = () -> {
+			System.out.println("First Name: ");
+			String pattern = SC.next();
+			if (Pattern.matches("^[A-Z]([a-z]{2,})$", pattern) == true)
+				return "Valid";
+			return "Invalid";
+		};
+
+		IUserRegistration lastName = () -> {
+			System.out.println("Last Name: ");
+			String pattern = SC.next();
+			if (Pattern.matches("^[A-Z]([a-z]{2,})$", pattern) == true)
+				return "Valid";
+			return "Invalid";
+		};
+
+		IUserRegistration email = () -> {
+			System.out.println("Email id: ");
+			String pattern = SC.next();
+			if (Pattern.matches("^[A-Za-z0-9]+([._%+-][0-9a-zA-Z]+)*@[A-Za-z0-9]+([.]([a-zA-Z]{2,3})*)+$",
+					pattern) == true)
+				return "Valid";
+			return "Invalid";
+		};
+
+		IUserRegistration mobileNum = () -> {
+			System.out.println("Mobile Number: ");
+			String pattern = SC.next();
+			if (Pattern.matches("^[0-9]{2,3}[: :]{1}[6-9]{1}[0-9]{9}$", pattern) == true)
+				return "Valid";
+			return "Invalid";
+		};
+
+		IUserRegistration password = () -> {
+			System.out.println("Password: ");
+			String pattern = SC.next();
+			if (Pattern.matches("^([0-9a-zA-Z@#!]){8,}$", pattern) == true) {
+				if (Pattern.matches("^([a-z0-9@#!]*)[A-Z]+([a-z0-9@#!]*)$", pattern) == true) {
+					if (Pattern.matches("^([a-zA-Z@#!]*)[0-9]+([a-zA-Z@#!]*)$", pattern) == true) {
+						if (Pattern.matches("^([a-zA-Z0-9]*)[@#!]{1}([a-zA-Z0-9]*)$", pattern) == true) {
+							return "All Rules Matched, Login Now!!";
+						}
+						return "Rule 4 Not Matched";
+					}
+					return "Rule 3 Not Matched";
+				}
+				return "Rule 2 Not Matched";
+			}
+			return "Rule 1 Not Matched";
+		};
+
+		System.out.println(firstName.validate());
+		System.out.println(lastName.validate());
+		System.out.println(email.validate());
+		System.out.println(mobileNum.validate());
+
+		System.out.println("Please follow the below rules to attain a valid password");
 		System.out.println("RULE-1 : Should contain minimum of 8 characters");
 		System.out.println("RULE-2 : Should have atleast 1 Uppercase letter");
 		System.out.println("RULE-3 : Should have atleast 1 numeric number in password");
 		System.out.println("RULE-4 : Should contain exactly 1 special character");
-		password();
+
+		System.out.println(password.validate());
 	}
 }
