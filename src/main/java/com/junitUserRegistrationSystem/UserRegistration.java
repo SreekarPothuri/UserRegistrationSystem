593f9403240b44d5d1a831171f668cfabef1544f
Your Name <you@example.com>
2020-10-29 22:44:22 +0530

    Refactored: All methods with Lambda Expression

--- src/main/java/com/junitUserRegistrationSystem/UserRegistration.java
+++ src/main/java/com/junitUserRegistrationSystem/UserRegistration.java
@@ -4,64 +4,59 @@ import java.util.regex.Pattern;
 
 import main.java.com.customexceptions.*;
 
+@FunctionalInterface
+interface IUserRegistration {
+	boolean validate(String pattern, String details);
+}
+
 public class UserRegistration {
-	
+
+	public static final String NAME_PATTERN = "^[A-Z]([a-z]{2,})$";
+	public static final String EMAIL_PATTERN = "^[A-Za-z0-9]+([._%+-][0-9a-zA-Z]+)*@[A-Za-z0-9]+([.]([a-zA-Z]{2,3})*)+$";
+	public static final String MOBILENUM_PATTERN = "^[0-9]{2,3}[: :]{1}[6-9]{1}[0-9]{9}$";
+	public static final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]*[@$!%&]([A-Za-z\\d]*){8,}$";
+
+	static IUserRegistration Obj = (detailsPattern, details) -> {
+		return (Pattern.matches(detailsPattern, details));
+	};
+
 	public static boolean firstName(String firstName) throws InvalidUserDetailsException {
-		boolean fname = Pattern.matches("^[A-Z]([a-z]{2,})$", firstName);
-		if(fname)
+		if (Obj.validate(NAME_PATTERN, firstName)) {
 			return true;
-		throw new InvalidUserDetailsException("Enter valid user details");
+		} else {
+			throw new InvalidUserDetailsException("Enter valid user details");
+		}
 	}
 
 	public static boolean lastName(String lastName) throws InvalidUserDetailsException {
-		boolean lname = Pattern.matches("^[A-Z]([a-z]{2,})$", lastName);
-		if(lname)
+		if (Obj.validate(NAME_PATTERN, lastName)) {
 			return true;
-		throw new InvalidUserDetailsException("Enter valid user details");
+		} else {
+			throw new InvalidUserDetailsException("Enter valid user details");
+		}
 	}
 
 	public static boolean email(String mail) throws InvalidUserDetailsException {
-		boolean email = Pattern.matches("^[A-Za-z0-9]+([._%+-][0-9a-zA-Z]+)*@[A-Za-z0-9]+([.]([a-zA-Z]{2,3})*)+$", mail);
-		if(email)
+		if (Obj.validate(EMAIL_PATTERN, mail)) {
 			return true;
-		throw new InvalidUserDetailsException("Enter valid user details");
-		
+		} else {
+			throw new InvalidUserDetailsException("Enter valid user details");
+		}
 	}
 
 	public static boolean mobileNum(String mNum) throws InvalidUserDetailsException {
-		boolean mobileNum = Pattern.matches("^[+]*([0-9]{1,3})+[[:space:]]+[0-9]{10}$", mNum);
-		if(mobileNum)
+		if (Obj.validate(MOBILENUM_PATTERN, mNum)) {
 			return true;
-		throw new InvalidUserDetailsException("Enter valid user details");
+		} else {
+			throw new InvalidUserDetailsException("Enter valid user details");
+		}
 	}
 
-	public static boolean password1(String password) throws InvalidUserDetailsException {
-		boolean pwd1 = Pattern.matches("^([0-9a-zA-Z@#!]){8,}$", password);
-		if(pwd1)
+	public static boolean password(String password) throws InvalidUserDetailsException {
+		if (Obj.validate(PASSWORD_PATTERN, password)) {
 			return true;
+		}else {
 		throw new InvalidUserDetailsException("Enter valid user details");
+		}
 	}
-
-	public static boolean password2(String password) throws InvalidUserDetailsException {
-		boolean pwd2 = Pattern.matches("^([a-z0-9@#!]*)[A-Z]+([a-z0-9@#!]*)$", password);
-		if(pwd2)
-			return true;
-		throw new InvalidUserDetailsException("Enter valid user details");
-	}
-
-	public static boolean password3(String password) throws InvalidUserDetailsException {
-		boolean pwd3 = Pattern.matches("^([a-zA-Z@#!]*)[0-9]+([a-zA-Z@#!]*)$", password);
-		if(pwd3)
-			return true;
-		throw new InvalidUserDetailsException("Enter valid user details");
-	}
-
-	public static boolean password4(String password) throws InvalidUserDetailsException {
-		boolean pwd4 = Pattern.matches("^([a-zA-Z0-9]*)[@#!$]{1}([a-zA-Z0-9]*)$", password);
-		if(pwd4)
-			return true;
-		throw new InvalidUserDetailsException("Enter valid user details");
-	}
-
-
 }
