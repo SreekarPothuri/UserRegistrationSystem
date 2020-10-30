22f61247e896b42f25e88f5d54c48839f8f090d0
SreekarPothuri <38813902+SreekarPothuri@users.noreply.github.com>
2020-10-30 17:07:36 +0530

    Delete InvalidUserDetailsException.java

--- InvalidUserDetailsException.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.customexceptions;
-
-public class InvalidUserDetailsException extends Exception {
-
-	public InvalidUserDetailsException(String message) {
-		super(message);
-	}
-}
